d21ed83da15b0b36cc944aac1669167666cb1237
Claude Wu <dwu1026@gmail.com>
2021-01-19 15:31:54 -0600

    :dog: Update Items in Containers(Tree Map)

--- src/Amazon/ItemInContainers/Main.java
+++ src/Amazon/ItemInContainers/Main.java
@@ -1,2 +1,24 @@
-package Amazon.ItemInContainers;public class Main {
+package Amazon.ItemInContainers;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] arg) {
+        Solution s = new Solution();
+
+        int[] testCase1 = s.numberOfItems("|**|*|*", new int[] {1, 1, 3}, new int[] {5, 6, 7});
+        int[] testCase2 = s.numberOfItems("*|*|", new int[] {1}, new int[] {3}); // in between two non-closed containers
+        int[] testCase3 = s.numberOfItems("****", new int[] {1}, new int[] {3}); // all stars
+        int[] testCase4 = s.numberOfItems("****|******|**", new int[] {6}, new int[] {13}); // all stars
+        int[] testCase5 = s.numberOfItems("|||*|||", new int[] {1}, new int[] {5}); // all stars
+
+        int[] testCase6 = s.numberOfItems2("|**|*|*", new int[] {1, 1, 3}, new int[] {5, 6, 7});
+
+        System.out.println(Arrays.toString(testCase1));
+        System.out.println(Arrays.toString(testCase2));
+        System.out.println(Arrays.toString(testCase3));
+        System.out.println(Arrays.toString(testCase4));
+        System.out.println(Arrays.toString(testCase5));
+        System.out.println(Arrays.toString(testCase6));
+    }
 }
--- src/Amazon/TransactionLogs/Main.java
+++ src/Amazon/TransactionLogs/Main.java
@@ -1,2 +1,15 @@
-package Amazon.TransactionLogs;public class Main {
+package Amazon.TransactionLogs;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] arg) {
+        Solution s = new Solution();
+        String[] testCase = {"345366 89921 45", "029323 38239 23",
+                "38239 345366 15", "029323 38239 77",
+                "345366 38239 23", "029323 345366 13",
+                "38239 38239 23"};
+
+        System.out.println(Arrays.toString(s.processLogFile(testCase, 3)));
+    }
 }
